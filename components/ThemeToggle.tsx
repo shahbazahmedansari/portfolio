f8b9e9d14c6d9d6b3049916712d75c5668b38cb8
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-12-28 18:28:56 +0530

    fix: Updated theme logic

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -10,7 +10,20 @@ export function ThemeToggle() {
     const { theme, setTheme } = useTheme();
 
     const switchTheme = () => {
-        theme === "light" ? setTheme("dark") : setTheme("light");
+        switch (theme) {
+            case "light":
+                setTheme("dark");
+                break;
+            case "dark":
+                setTheme("light");
+                break;
+            default:
+                break;
+        }
+    };
+
+    const toggleTheme = () => {
+        switchTheme();
     };
 
     return (
@@ -18,7 +31,7 @@ export function ThemeToggle() {
             variant="outline"
             size="icon"
             className="rounded-full hover:cursor-pointer hover:shadow-xl p-4"
-            onClick={switchTheme}
+            onClick={toggleTheme}
         >
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 text-blue-700 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] text-blue-700 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
